f4b4412e5e4c5027e608b08b96576edbc81b3c38
Airusa9 <fairy.tail.of.nero@gmail.com>
2024-09-13 01:54:54 +0700

    Add files via upload

---
+++ zcon.py
@@ -0,0 +1,55 @@
+__author__      = "Muhammad Quwais Saputra"
+__version__     = "0.1"
+
+# font
+class font:
+	black   = "\033[30m"
+	red     = "\033[31m"
+	green   = "\033[32m"
+	yellow  = "\033[33m"
+	blue    = "\033[34m"
+	purple  = "\033[35m"
+	pink    = "\033[35m"
+	magenta = "\033[35m"
+	cyan    = "\033[36m"
+	
+	# light
+	lightgrey   = "\033[37m"
+	lightred    = "\033[91m"
+	lightgreen  = "\033[92m"
+	lightyellow = "\033[93m"
+	lightblue   = "\033[94m"
+	lightpurple = "\033[95m"
+	lightmagenta= "\033[95m"
+	lightpink   = "\033[95m"
+	lightcyan   = "\033[96m"
+	
+	# white or normal
+	white = "\033[97m"
+	normal = "\033[97m"
+	
+# background
+class Bg:
+	# normal
+	black   = "\033[40m"
+	normal  = "\033[40m"
+	
+	red     = "\033[41m"
+	green   = "\033[42m"
+	yellow  = "\033[43m"
+	blue    = "\033[44m"
+	purple  = "\033[45m"
+	pink    = "\033[45m"
+	magenta = "\033[45m"
+	cyan    = "\033[46m"
+	
+	# light
+	lightgrey    = "\033[47m"
+	lightred     = "\033[101m"
+	lightgreen   = "\033[102m"
+	lightyellow  = "\033[103m"
+	lightblue    = "\033[104m"
+	lightpurple  = "\033[105m"
+	lightpink    = "\033[106m"
+	lightmagenta = "\033[107m"
+	lightcyan    = "\033[108m"
